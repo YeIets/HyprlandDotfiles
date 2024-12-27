2644b8a469383053c0ed84b9c0e2e5a27208b3db
Yelets <nelsonquiew@gmail.com>
2024-12-26 18:39:05 -0600

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,2 +1,27 @@
 #!/usr/bin/bash
-currentDir=$(pwd)yayURL="https://aur.archlinux.org/yay.git"
+
+
+_startServices(){
+
+	for item in "${Services[@]}"; do
+
+		echo -e "${GREEN}"
+		echo "Enabling and starting $item service"
+
+		echo systemctl enable "$item"".service";
+		if [[ "$item" == ly ]]; then
+			:
+        else
+		    echo systemctl start "$item"".service";
+        fi
+	done;
+
+	echo -e "${WHITE}"
+}
+
+Services=("ly"
+		  "NetworkManager"
+		  "bluetooth"
+)
+
+_startServices
